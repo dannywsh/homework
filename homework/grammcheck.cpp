88d82665eecda025437ef86199f6bebedada853e
Wang SH <0309wsh@163.com>
2020-11-13 17:44:36 +0800

    need to add print part & fix parenthese part and semicolon part

--- homework/grammcheck.cpp
+++ homework/grammcheck.cpp
@@ -9,14 +9,16 @@
 
 #define ErrNumber   100
 #define ErrClass   10
+#define ERR		1
+
+#define LeftComment 0
+#define RightComment 1
 
 using namespace std;
 
-const char* SourceFile = "C:\\Users\\Administrator\\Desktop\\1.txt";
-const char* TargetFile = "C:\\Users\\Administrator\\Desktop\\2.txt";
-const char* OutputFile = "C:\\Users\\Administrator\\Desktop\\out.txt";
-
-
+const char* SrcFile = "C:\\Users\\Administrator\\Desktop\\1.txt";
+const char* TrgFile = "C:\\Users\\Administrator\\Desktop\\2.txt";
+const char* OutFile = "C:\\Users\\Administrator\\Desktop\\out.txt";
 
 class CGramCheck
 {
@@ -24,32 +26,37 @@ private:
 	int textline;   //文件总行数
 	int errSum;	//错误总数
 	vector<int>	curlyBracesError;
+	vector<vector<int> >	commentError;
 public:
 	CGramCheck();
-	void GetTextline(ifstream& fin);            //获取文件总行数
-	void CommentCheck(ifstream& fin);   //对注释内容进行检查，并找出错误 
-	void ParentheseCheck(ifstream& fin);	//检查括号错误，返回错误总数
-	void SemicolonCheck(ifstream& fin);	//分号检查
+	int GetTextline(const char* Sourcefile);            //获取文件总行数
+	int CommentCheck(const char* Sourcefile, const char* Tempfile);   //对注释内容进行检查，并找出错误 
+	int ParentheseCheck(const char* Tempfile);	//检查括号错误，返回错误总数
+	int SemicolonCheck(const char* Tempfile);	//分号检查
 	static bool isblank(char i) { return i == ' ' || i == '\t'; }
 };
 
 int main()
 {
 	CGramCheck MyCheck;
-	ifstream fin; ofstream fout;    //输入输出文件流
+	/*
 	fin.open(SourceFile);
+	fout.open(OutputFile, ios::out | ios::trunc);
 	if (!fin) {
 		cout << "Can't open file " << SourceFile << "!" << endl;
 		return -1;
 	}
-	else {
-		MyCheck.GetTextline(fin);
-		MyCheck.CommentCheck(fin);
-		MyCheck.ParentheseCheck(fin);
-		MyCheck.SemicolonCheck(fin);
-		fin.close();
+	if (!fout) {
+		cout << "Can't open file " << OutputFile << "!" << endl;
+		return -1;
 	}
+	*/
+	MyCheck.GetTextline(SrcFile);
+	MyCheck.CommentCheck(SrcFile,TrgFile);
+	MyCheck.ParentheseCheck(TrgFile);
+	MyCheck.SemicolonCheck(TrgFile);
 }
+
 CGramCheck::CGramCheck()
 {
 	textline = 0;
@@ -57,15 +64,156 @@ CGramCheck::CGramCheck()
 	vector<vector<int>>commentFlag(0, vector<int>(2));
 }//初始化构造函数
 
-void CGramCheck::GetTextline(ifstream& fin)
+int CGramCheck::GetTextline(const char* Sourcefile)
 {
 	string temp;
+	ifstream fin;
+	fin.open(Sourcefile);
+	if (!fin) {
+		cout << "Can't open file " << Sourcefile << "!" << endl;
+		return ERR;
+	}
 	while (getline(fin, temp)) { textline++; }
-	fin.clear();
-	fin.seekg(0, ios::beg);
+	fin.close();
+	return 0;
 }
 
-void CGramCheck::ParentheseCheck(ifstream& fin)
+int CGramCheck::CommentCheck(const char* Sourcefile, const char* Tempfile) {
+	string temp;
+	ifstream fin;ofstream fout;
+
+	bool doublequotes = false;
+	bool singlequotes = false;
+	bool comflag = false;
+	int line(0);
+
+	fin.open(Sourcefile);
+	fout.open(Tempfile, ios::out | ios::trunc);
+	if (!fin) {
+		cout << "Can't open file " << Sourcefile << "!" << endl;
+		return -1;
+	}
+	if (!fout) {
+		cout << "Can't open file " << Tempfile << "!" << endl;
+		return -1;
+	}
+
+	while (getline(fin, temp)) {
+		++line;
+		int i(0);
+		if (!temp.length()) {
+			fout << endl;
+			continue;
+		}
+		while (i < temp.length() - 1) {
+			if (temp[i] == '\"') {
+				if (!comflag) {
+					if (singlequotes) {
+						i++;
+					}
+					else {
+						doublequotes = !doublequotes;
+						fout << temp[i];
+						i++;
+					}
+				}
+				else {
+					i++;
+				}
+			}
+			else if (temp[i] == '\'') {
+				if (!comflag) {
+					if (doublequotes) {
+						i++;
+					}
+					else {
+						singlequotes = !singlequotes;
+						fout << temp[i];
+						i++;
+					}
+				}
+				else {
+					i++;
+				}
+			}
+			else if (temp[i] == '/' && temp[i + 1] == '*') {
+				if ((!doublequotes) && (!singlequotes) && !comflag) {
+					i += 2;
+					comflag = true;
+				}
+				else if ((!doublequotes) && (!singlequotes) && comflag) {
+					i++;
+				}
+				else if (doublequotes || singlequotes) {
+					//fout << temp[i];
+					i++;
+				}
+				else {
+					cout << "还有这种情况？？？" << endl;
+				}
+			}
+			else if (temp[i] == '*' && temp[i + 1] == '/') {
+				if ((!doublequotes) && (!singlequotes) && !comflag) {
+					cout << "缺少/*"<<endl;
+					vector<int>tp;
+					tp.push_back(line);
+					tp.push_back(LeftComment);
+					commentError.push_back(tp);
+					fout << temp[i];
+					i++;
+				}
+				else if ((!doublequotes) && (!singlequotes) && comflag) {
+					i += 2;
+					comflag = false;
+				}
+				else if (doublequotes || singlequotes) {
+					//fout << temp[i];
+					i++;
+				}
+				else {
+					cout << "还有这种情况？？？" << endl;
+				}
+			}
+			else if (temp[i] == '/' && temp[i + 1] == '/') {
+				if ((!doublequotes) && (!singlequotes))break;
+				else {
+					//fout << temp[i];
+					i++;
+				}
+			}
+			else if (temp[i] == '\\') {
+				i += 2;
+			}
+			else {
+				if (!comflag && !singlequotes && !doublequotes) {
+					fout << temp[i];
+				}
+				i++;
+			}
+		}
+		if (i == temp.length() - 1)fout << temp[i];
+		fout << endl;
+		if (doublequotes) {
+			cout << "缺少双引号！" << line << endl;
+			doublequotes = false;
+		}
+		if (singlequotes) {
+			cout << "缺少单引号！" << line << endl;
+			singlequotes = false;
+		}
+	}
+	if (comflag) {
+		cout << "缺少*/" << endl;
+		vector<int>tp;
+		tp.push_back(line);
+		tp.push_back(RightComment);
+		commentError.push_back(tp);
+	}
+	fin.close();
+	fout.close();
+}
+
+void CGramCheck::ParentheseCheck(const char* Tempfile, const char* Outputfile)
 {
 	string temp;
 	int line(0);
@@ -128,121 +276,8 @@ void CGramCheck::ParentheseCheck(ifstream& fin)
 	fin.clear();
 	fin.seekg(0, ios::beg);
 }
-void CGramCheck::CommentCheck(ifstream& fin) {
-	string temp;
-	ofstream fout;
-	bool doublequotes = false;
-	bool singlequotes = false;
-	bool comflag = false;
 
-	fout.open(TargetFile, ios::out | ios::trunc);
-	int line(0);
-
-	while (getline(fin, temp)) {
-		++line;
-		int i(0);
-		if (!temp.length()) {
-			fout << endl;
-			continue;
-		}
-		while (i < temp.length() - 1) {
-			if (temp[i] == '\"') {
-				if (!comflag) {
-					if (singlequotes) {
-						i++;
-					}
-					else {
-						doublequotes = !doublequotes;
-						fout << temp[i];
-						i++;
-					}
-				}
-				else {
-					i++;
-				}
-			}
-			else if (temp[i] == '\'') {
-				if (!comflag) {
-					if (doublequotes) {
-						i++;
-					}
-					else {
-						singlequotes = !singlequotes;
-						fout << temp[i];
-						i++;
-					}
-				}
-				else{
-					i++;
-				}
-			}
-			else if (temp[i] == '/' && temp[i + 1] == '*') {
-				if ((!doublequotes) && (!singlequotes) && !comflag) {
-					i += 2;
-					comflag = true;
-				}
-				else if ((!doublequotes) && (!singlequotes) && comflag) {
-					i++;
-				}
-				else if (doublequotes || singlequotes) {
-					//fout << temp[i];
-					i++;
-				}
-				else {
-					cout << "还有这种情况？？？" << endl;
-				}
-			}
-			else if (temp[i] == '*' && temp[i + 1] == '/') {
-				if ((!doublequotes) && (!singlequotes) && !comflag) {
-					cout << "前注释不匹配！";
-					fout << temp[i];
-					i++;
-				}
-				else if ((!doublequotes) && (!singlequotes) && comflag) {
-					i += 2;
-					comflag = false;
-				}
-				else if (doublequotes || singlequotes) {
-					//fout << temp[i];
-					i++;
-				}
-				else {
-					cout << "还有这种情况？？？" << endl;
-				}
-			}
-			else if (temp[i] == '/' && temp[i + 1] == '/') {
-				if ((!doublequotes) && (!singlequotes))break;
-				else {
-					//fout << temp[i];
-					i++;
-				}
-			}
-			else if (temp[i] == '\\') {
-				i += 2;
-			}
-			else {
-				if (!comflag	&& !singlequotes && !doublequotes) {
-					fout << temp[i];
-				}
-				i++;
-			}
-		}
-		if (i == temp.length() - 1)fout << temp[i];
-		fout << endl;
-		if (doublequotes) {
-			cout << "缺少双引号！" << line << endl;
-			doublequotes = false;
-		} 
-		if (singlequotes) {
-			cout << "缺少单引号！" << line << endl;
-			singlequotes = false;
-		}
-	}
-	fin.clear();
-	fin.seekg(0, ios::beg);
-	fout.close();
-}
-void CGramCheck::SemicolonCheck(ifstream& fin)
+void CGramCheck::SemicolonCheck(const char* Tempfile, const char* Outputfile)
 {
 	string temp; int line(0);
 	while (getline(fin, temp)) {
